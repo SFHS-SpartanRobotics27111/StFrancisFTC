abe814f94942905d71e572a3775592a6cbe2400c
Munson <25NoMunson@SFHSStudents.org>
2024-11-19 15:55:40 -0600

    Arm positions added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
@@ -9,21 +9,23 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 public class Arm {
     private static final String armName = "left_arm";
-
-    final double ARM_TICKS_PER_DEGREE =
-            28
-                * 250047.0 / 4913.0
-                * 100.0 / 20.0
-                * 1 / 360.0;
-
-    final double ARM_COLLAPSED_IN = 0;
-    double armPosition = (int)ARM_COLLAPSED_IN;
     private static final double ARM_UP = 0.45;
     private static final double ARM_DOWN = -0.45;
-
-    public DcMotor arm;
-
+    final double ARM_TICKS_PER_DEGREE =
+            28
+                    * 250047.0 / 4913.0
+                    * 100.0 / 20.0
+                    * 1 / 360.0;
+    final double ARM_COLLAPSED_IN = 0;
+    final double ARM_COLLECT = 250 * ARM_TICKS_PER_DEGREE;
+    final double ARM_CLEAR_BARRIER = 230 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SPECIMEN = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SAMPLE_IN_LOW = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_ATTACH_HANGING_HOOK = 120 * ARM_TICKS_PER_DEGREE;
+    final double ARM_WINCH_ROBOT = 15 * ARM_TICKS_PER_DEGREE;
     private final Telemetry telemetry;
+    public DcMotor arm;
+    double armPosition = (int) ARM_COLLAPSED_IN;
 
     public Arm(HardwareMap hardwareMap, Telemetry telemetry1) {
         telemetry = telemetry1;
@@ -36,7 +38,7 @@ public class Arm {
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
-    /*public void moveArm(boolean up, boolean down, boolean stop) {
+    public void moveArm(boolean up, boolean down, boolean stop) {
         if (up) {
             arm.setPower(ARM_UP);
             telemetry.addData("Arm", "Moving up");
@@ -47,29 +49,35 @@ public class Arm {
             arm.setPower(0.0);
             telemetry.addData("Arm", "Stopping");
         }
-    }*/
+    }
 
-    public void moveArmWithEncoder(boolean up, boolean down) {
-        if (up) {
-            armPosition += 1 * ARM_TICKS_PER_DEGREE;
+    public void moveArmWithEncoder(boolean a, boolean b, boolean x, boolean y, boolean up, boolean down, boolean left) {
+        if (a) {
+            armPosition = ARM_COLLECT;
             telemetry.addData("Arm position: ", arm.getCurrentPosition());
-            telemetry.update();
+        } else if (b) {
+            armPosition = ARM_CLEAR_BARRIER;
+            telemetry.addData("Arm position: ", arm.getCurrentPosition());
+        } else if (x) {
+            armPosition = ARM_SCORE_SPECIMEN;
+            telemetry.addData("Arm position: ", arm.getCurrentPosition());
+        } else if (y) {
+            armPosition = ARM_WINCH_ROBOT;
+            telemetry.addData("Arm position: ", arm.getCurrentPosition());
+        } else if (up) {
+            armPosition = ARM_COLLAPSED_IN;
+            telemetry.addData("Arm position: ", arm.getCurrentPosition());
         } else if (down) {
-            armPosition -= 1 * ARM_TICKS_PER_DEGREE;
+            armPosition = ARM_ATTACH_HANGING_HOOK;
             telemetry.addData("Arm position: ", arm.getCurrentPosition());
-            telemetry.update();
+        } else if (left) {
+            armPosition = ARM_SCORE_SAMPLE_IN_LOW;
+            telemetry.addData("Arm position: ", arm.getCurrentPosition());
         }
 
         if (((DcMotorEx) arm).isOverCurrent()) {
             telemetry.addLine("MOTOR EXCEEDED CURRENT LIMIT!!");
-            telemetry.update();
         }
     }
 }
-/* Positions to add:
-grabbing sample
-getting into the submersible
-folded in
-scoring sample
-scoring specimen
- */
+
