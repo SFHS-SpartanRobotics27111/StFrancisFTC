0c07af6acc321c0a3fccd7f1624bb1e9ef325069
noahmunson <noahthemunson@gmail.com>
2025-01-21 15:16:06 -0600

    FIRST ARM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SplineTestMine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SplineTestMine.java
@@ -5,7 +5,7 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
+import org.firstinspires.ftc.teamcode.hardware.Arm;
 import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
 @Autonomous(name="Lazarus", group="Linear OpMode")
 public final class SplineTestMine extends LinearOpMode {
@@ -14,6 +14,7 @@ public final class SplineTestMine extends LinearOpMode {
         Pose2d beginPose = new Pose2d(-24, 58, 0);
 
             PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
+            Arm arm = new Arm(hardwareMap, telemetry);
 
             waitForStart();
 
@@ -22,6 +23,8 @@ public final class SplineTestMine extends LinearOpMode {
 
                             .lineToX(38)
                             .splineTo(new Vector2d(38, 24), Math.PI / 2)
+                            arm.moveArmWithEncoder(ARMC)
+
                             .build());
         }
 
