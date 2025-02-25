b90d56a6eaf4cf591cc5b285a16acf374ad92e5a
noahmunson <noahthemunson@gmail.com>
2025-02-24 21:21:01 -0800

    Functioning Pathing in MeepMeepTesting

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,7 +1,9 @@
 
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.Actions;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -10,7 +12,7 @@ import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        Pose2d beginPose = new Pose2d(0, 48, Math.PI / 2);
+        Pose2d beginPose = new Pose2d(10, -65, -Math.PI);
 
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -18,9 +20,24 @@ public class MeepMeepTesting {
                 .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(beginPose)
-                .splinetoLinearHeading(new Pose2d(-46, 9, Math.PI / 2), -Math.PI / 2)
-                );
+        TrajectoryActionBuilder toChamber = myBot.getDrive().actionBuilder(beginPose)
+                .strafeToLinearHeading(new Vector2d(0, -42), Math.PI / 2);
+
+        TrajectoryActionBuilder Backup = myBot.getDrive().actionBuilder(new Pose2d(0, -42, Math.PI / 2))
+                .lineToY(-65);
+
+        TrajectoryActionBuilder SplineToPush = myBot.getDrive().actionBuilder(new Pose2d(0, -65, Math.PI / 2))
+                .setTangent(0)
+                .splineToLinearHeading(new Pose2d(46, -6, -Math.PI / 2), -Math.PI / 2);
+
+
+        myBot.runAction(
+                new SequentialAction(
+                toChamber.build(),
+                        Backup.build(),
+                        SplineToPush.build()
+                )
+        );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
                 .setDarkMode(true)
